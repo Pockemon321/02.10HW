d42b5203f9715048c81876b13f3eb99baf21f023
Pockemon321 <64663973+Pockemon321@users.noreply.github.com>
2023-10-02 11:56:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById('toggle');
+const text = document.getElementById('text');
+
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active');
+});
